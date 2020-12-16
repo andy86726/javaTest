27fbd4c9530b35554a08b9b5e72dbd8a7c8d9a68
ful67485 <ful67485@gmail.com>
2020-12-16 15:56:56 +0800

    test

---
+++ Test1.java
@@ -0,0 +1,19 @@
+package date1119;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str = "JAVA";
+int num = 8;
+double ver = 1.8005;
+System.out.printf("I love %s. We use %s %d %5.1f .", str, str, num, ver);
+
+String str2 = "YVTS";
+double num2 = 1.83;
+String str3 = "\\";
+System.out.printf("%s %4.1f  %s", str2, num2, str3); 
+
+	}
+
+}
